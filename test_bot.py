be899301ae648129ed42cef946b4a1cd45d78e37
YR Aprova <lincolnsimpson2005@gmail.com>
2026-08-29 14:08:37 -0300

    atualiza test_bot.py com encoding correto

--- test_bot.py
+++ test_bot.py
@@ -1,9 +1,10 @@
+ï»¿# -*- coding: utf-8 -*-
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Olá! O bot está ativo.')
+    await update.message.reply_text('Bot ativo com sucesso.')
 
 def main():
     token = '8988706536:AAElQEEH-LouWkLa5YNW_k0isUcUGzm0jps'
