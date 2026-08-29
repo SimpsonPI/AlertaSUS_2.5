39df9a786400282782f0ddda0a8b2474f0248b51
YR Aprova <lincolnsimpson2005@gmail.com>
2026-08-29 14:05:54 -0300

    popula o arquivo test_bot.py corretamente

---
+++ test_bot.py
@@ -0,0 +1,15 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Olá! O bot está ativo.')
+
+def main():
+    token = '8988706536:AAElQEEH-LouWkLa5YNW_k0isUcUGzm0jps'
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler('start', start))
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
